c544e02df9259f194d0fc3dbdb9edd2af72c0dd5
neku <hoshi1682@gmail.com>
2021-07-18 23:03:42 +0900

    Add Report create

--- WebContent/WEB-INF/views/reports/new.jsp
+++ WebContent/WEB-INF/views/reports/new.jsp
@@ -4,11 +4,9 @@
 <c:import url="/WEB-INF/views/layout/app.jsp">
     <c:param name="content">
         <h2>日報  新規登録ページ</h2>
-
         <form method="POST" action="<c:url value='/reports/create' />" >
             <c:import url="_form.jsp" />
-        </form>>
-
+        </form>
         <p><a href="<c:url value='/reports/index' />">一覧に戻る</a></p>
     </c:param>
 </c:import>
--- src/controllers/reports/ReportsCreateServlet.java
+++ src/controllers/reports/ReportsCreateServlet.java
@@ -1,19 +1,30 @@
 package controllers.reports;
 
 import java.io.IOException;
+import java.sql.Date;
+import java.sql.Timestamp;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import models.Employee;
+import models.Report;
+import models.validators.ReportValidator;
+import utils.DBUtil;
+
 /**
  * Servlet implementation class ReportsCreateServlet
  */
 @WebServlet("/reports/create")
 public class ReportsCreateServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
+    private static final long serialVersionUID = 1L;
+
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -22,11 +33,58 @@ public class ReportsCreateServlet extends HttpServlet {
         // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-	}
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String _token = request.getParameter("_token");
+        if(_token != null && _token.equals(request.getSession().getId())) {
+            EntityManager em = DBUtil.createEntityManager();
+
+            Report r = new Report();
+
+            // ログイン情報を取得
+            r.setEmployee((Employee)request.getSession().getAttribute("login_employee"));
+
+
+            // 日付の処理
+            Date report_date = new Date(System.currentTimeMillis());
+            String rd_str = request.getParameter("report_date");
+            // 日付覧が未入力の場合、当日の日付を入れる(バリデーションで必須入力チェックでもいい)
+            if(rd_str != null && !rd_str.equals("")) {
+                // 取得した日付をDate型に変換
+                report_date = Date.valueOf(request.getParameter("report_date"));
+            }
+            r.setReport_date(report_date);
+
+            r.setTitle(request.getParameter("title"));
+            r.setContent(request.getParameter("content"));
+
+            Timestamp currentTime = new Timestamp(System.currentTimeMillis());
+            r.setCreated_at(currentTime);
+            r.setUpdated_at(currentTime);
+
+            List<String> errors = ReportValidator.validate(r);
+            if(errors.size() > 0) {
+                em.close();
+
+                request.setAttribute("_token", request.getSession().getId());
+                request.setAttribute("report", r);
+                request.setAttribute("errors", errors);
+
+                RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/reports/new.jsp");
+                rd.forward(request, response);
+            } else {
+                em.getTransaction().begin();
+                em.persist(r);
+                em.getTransaction().commit();
+                em.close();
+                request.getSession().setAttribute("flush", "登録が完了しました");
+
+                response.sendRedirect(request.getContextPath() + "/reports/index");
+
+            }
+        }
+    }
 
 }
